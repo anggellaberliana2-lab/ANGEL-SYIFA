2ae7a9f5f08d69fc78fd6c52959b86b08ee762cb
angel <anggellaberliana2@gmail.com>
2025-11-18 23:02:04 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,63 @@
+// Data dummy untuk bahan ajar
+const dataMenu = [
+    {
+        idKue: "K01",
+        namaKue: "Fluffie Bunnie",
+        deskripsi: "Cupcake marshmallow dengan topping warna pastel",
+        status: "Tersedia",
+        stok: 50
+    },
+    {
+        idKue: "K02",
+        namaKue: "Berrylicious",
+        deskripsi: "Tart kecil berisi krim dan buah beri segar, rasa segar dan manisnya pas banget!",
+        status: "Tersedia",
+        stok: 30
+    },
+    {
+        idKue: "K03",
+        namaKue: "Choco Cloud",
+        deskripsi: "Mousse cokelat lembut yang langsung meleleh di mulut",
+        status: "Terbatas",
+        stok: 25
+    },
+    {
+        idKue: "K04",
+        namaKue: "Butter Bliss",
+        deskripsi: "Kue kering mentega yang renyah di luar dan lumer di lidah",
+        status: "Habis",
+        stok: 0
+    },
+    {
+        idKue: "K05",
+        namaKue: "Honey Hug",
+        deskripsi: "Kue kering madu yang rasanya hangat",
+        status: "Tersedia",
+        stok: 42
+    }
+];
+
+// Data dummy untuk tracking pengiriman
+const dataPengiriman = [
+    {
+        idPesanan: "PD6782",
+        namaPelanggan: "Nuel",
+        status: "Dikirim",
+        progress: 75,
+        tanggalKirim: "20-09-2024",
+    },
+    {
+        idPesanan: "PD880925",
+        namaPelanggan: "Matthew",
+        status: "Diproses",
+        progress: 45,
+        tanggalKirim: "15-04-2025",
+    },
+    {
+        idPesanan: "PD11025",
+        namaPelanggan: "Jueyo",
+        status: "Selesai",
+        progress: 100,
+        tanggalKirim: "31-02-2026",
+    }
+];
